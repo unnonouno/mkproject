3a9d8ea6ab60a37bd1bcc13aa3f34b2dc284c336
Yuya Unno <unnonouno@gmail.com>
2014-03-01 14:48:21 +0900

    Fix code style

--- mkproject.sh
+++ mkproject.sh
@@ -46,48 +46,48 @@ VERSION = '0.0.0'
 APPNAME = 'appname'
 
 def options(opt):
-  opt.load('compiler_cxx')
-  opt.load('unittest_gtest')
+    opt.load('compiler_cxx')
+    opt.load('unittest_gtest')
 
 def configure(conf):
-  conf.env.CXXFLAGS += ['-O2', '-Wall', '-g', '-pipe']
-  conf.load('compiler_cxx')
-  conf.load('unittest_gtest')
+    conf.env.CXXFLAGS += ['-O2', '-Wall', '-g', '-pipe']
+    conf.load('compiler_cxx')
+    conf.load('unittest_gtest')
 
-  # conf.check_cfg(package = 'pficommon', args = '--cflags --libs')
-  # conf.check_cxx(lib='libname', header_name = 'header.h')
+    # conf.check_cfg(package='pficommon', args='--cflags --libs')
+    # conf.check_cxx(lib='libname', header_name='header.h')
 
 def build(bld):
-  bld.program(
-    source = 'source.cpp',
-    target = 'target',
-    use = ''
-  )
+    bld.program(
+        source='source.cpp',
+        target='target',
+        use=''
+    )
 
 def cpplint(ctx):
-  cpplint_args = '--filter=-runtime/references,-legal/copyright,-build/include_order --extensions=cpp,hpp'
+    cpplint_args = '--filter=-runtime/references,-legal/copyright,-build/include_order --extensions=cpp,hpp'
 
-  src_dir = ctx.path.find_node('src')
-  files = []
-  for f in src_dir.ant_glob('**/*.cpp **/*.hpp'):
-    files.append(f.path_from(ctx.path))
+    src_dir = ctx.path.find_node('src')
+    files = []
+    for f in src_dir.ant_glob('**/*.cpp **/*.hpp'):
+        files.append(f.path_from(ctx.path))
 
-  args = 'cpplint.py %s %s' % (cpplint_args,' '.join(files))
-  result = ctx.exec_command(args)
-  if result != 0:
-    ctx.fatal('cpplint failed')
+    args = 'cpplint.py %s %s' % (cpplint_args,' '.join(files))
+    result = ctx.exec_command(args)
+    if result != 0:
+        ctx.fatal('cpplint failed')
 
-def gcovr(ctx):
-  excludes = [
-    '.*\\.unittest-gtest.*',
-    '.*_test\\.cpp'
-    ]
+  def gcovr(ctx):
+      excludes = [
+          '.*\\.unittest-gtest.*',
+          '.*_test\\.cpp',
+        ]
 
-  args = 'gcovr --branches -r . '
-  for e in excludes:
-    args += ' -e "%s"' % e
+      args = 'gcovr --branches -r . '
+      for e in excludes:
+          args += ' -e "%s"' % e
 
-  ctx.exec_command(args)
+      ctx.exec_command(args)
 EOF
 fi
 
